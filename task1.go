346a8cd32aaeeeb057c96476090b9c42f7166232
Hammad Ahmed ~ <75477054+hammadahmedpk@users.noreply.github.com>
2022-09-22 11:12:32 +0500

    Update task1.go

--- task1.go
+++ task1.go
@@ -12,10 +12,31 @@ type Person struct {
 }
 
 // Printing the person's values
-func PrintStruc(person Person) {
-	fmt.Println("Name: ", person.name)
-	fmt.Println("Age: ", person.age)
-	fmt.Println("Job: ", person.job)
-	fmt.Println("Salary: ", person.salary)
+func PrintStruc() {
+	var pers1 Person
+	var pers2 Person
+
+	// Pers1 specification
+	pers1.name = "Hege"
+	pers1.age = 45
+	pers1.job = "Teacher"
+	pers1.salary = 6000
+
+	// Pers2 specification
+	pers2.name = "Cecilie"
+	pers2.age = 24
+	pers2.job = "Marketing"
+	pers2.salary = 4500
+	
+	fmt.Println("Name: ", pers1.name)
+	fmt.Println("Age: ", pers1.age)
+	fmt.Println("Job: ", pers1.job)
+	fmt.Println("Salary: ", pers1.salary)
+	fmt.Println()
+
+	fmt.Println("Name: ", pers2.name)
+	fmt.Println("Age: ", pers2.age)
+	fmt.Println("Job: ", pers2.job)
+	fmt.Println("Salary: ", pers2.salary)
 	fmt.Println()
 }
